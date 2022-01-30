21ae19ed361344720478e01751d832ef9bc4eb82
Mishal <mishal0404@gmail.com>
2022-01-30 09:56:36 +0530

    dsfsdf

---
+++ hello/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("HelloKKKKK")
+
+def david(request):
+    return HttpResponse("Hello david")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
